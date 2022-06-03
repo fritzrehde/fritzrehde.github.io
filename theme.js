55a82f90bf296694735f41769af6381ec0da0965
Fritz Rehde <fritzrehde.work@gmail.com>
2022-06-03 16:16:10 +0200

    Save last used color theme to sessionstorage instead of localstorage

--- theme.js
+++ theme.js
@@ -4,40 +4,22 @@ function showBody(bool) {
 }
 
 function saveTheme(theme) {
-	localStorage.setItem("theme", theme);
+	sessionStorage.setItem("theme", theme);
 }
 
 function getTheme() {
-	return localStorage.getItem("theme");
+	return sessionStorage.getItem("theme");
 }
 
 function initTheme() {
-	var theme;
-	if (theme = getTheme()) {
+	var theme = getTheme();
+	if (theme) {
 		setTheme(theme);
 	} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
 		setTheme('dark');
 	}
 }
 
-function saveTheme(theme) {
-	localStorage.setItem("theme", theme);
-}
-
-function getTheme() {
-	return localStorage.getItem("theme");
-}
-
-function initTheme() {
-	var theme = getTheme();
-	if (!theme) {
-		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-			setTheme('dark');
-		}
-	}
-	setTheme(theme);
-}
-
 function setTheme(theme) {
 	var icon = (theme === 'light') ? 'bi-toggle-off' : 'bi-toggle-on';
 	document.body.className = theme;
@@ -46,27 +28,9 @@ function setTheme(theme) {
 }
 
 function toggleTheme() {
-	console.log("toggleTheme before", document.body.classList, getTheme());
-}
-
-function setTheme(theme) {
-	var icon = (theme === 'light') ? 'bi-toggle-off' : 'bi-toggle-on';
-	document.body.className = theme;
-	document.getElementById("toggle-icon").className = icon;
-	saveTheme(theme);
-}
-
-function toggleTheme() {
-	console.log("toggleTheme before", document.body.classList, getTheme());
-
 	if (document.body.classList.contains('dark')) {
 		setTheme('light');
 	} else {
 		setTheme('dark');
 	}
-
-	console.log("toggleTheme after", document.body.classList, getTheme());
-
-	// document.body.classList.toggle("dark");
-	// document.getElementById("toggle-icon").classList.toggle('bi-toggle-on');
 }

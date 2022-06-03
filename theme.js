f51ceff0b800bfccab4cffa52011b71c4e28fe61
Fritz Rehde <fritzrehde.work@gmail.com>
2022-06-03 11:25:21 +0200

    Small button styling fix

--- theme.js
+++ theme.js
@@ -2,7 +2,7 @@ function toggle_theme() {
 	var element = document.body;
 	element.classList.toggle("alternate-theme");
 
-	var button = document.getElementById("theme-button");
+	var button = document.getElementById("toggle-icon");
 	button.classList.toggle('bi-toggle-on');
 }
 

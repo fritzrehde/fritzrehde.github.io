98c4534379156d4ae0fc80c2dd41591aeb119f94
Fritz Rehde <fritzrehde.work@gmail.com>
2022-06-03 14:11:01 +0200

    Fixed flickering bug when loading dark theme by default

--- theme.js
+++ theme.js
@@ -1,10 +1,19 @@
-window.onload = function() {
-	if (window.matchMedia("(prefers-color-scheme: dark)").matches)
-		toggle_theme();
+function hideBody() {
+	document.body.style.visibility = 'hidden';
+	document.body.style.opacity = 0;
 }
 
-function toggle_theme() {
-	document.body.classList.toggle("alternate-theme");
+function showBody() {
+	document.body.style.visibility = 'visible';
+	document.body.style.opacity = 1;
+}
+
+function initTheme() {
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+		toggleTheme();
+}
+
+function toggleTheme() {
+	document.body.classList.toggle("dark");
 	document.getElementById("toggle-icon").classList.toggle('bi-toggle-on');
 }
-

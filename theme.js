d3c5696647049c9b31e01101d80e1f30660cd9de
Fritz Rehde <fritzrehde.work@gmail.com>
2022-06-03 15:57:00 +0200

    Save last used color theme to localstorage

--- theme.js
+++ theme.js
@@ -1,19 +1,72 @@
-function hideBody() {
-	document.body.style.visibility = 'hidden';
-	document.body.style.opacity = 0;
+function showBody(bool) {
+	document.body.style.visibility = (bool) ? 'visible' : 'hidden';
+	document.body.style.opacity = (bool) ? 1 : 0;
 }
 
-function showBody() {
-	document.body.style.visibility = 'visible';
-	document.body.style.opacity = 1;
+function saveTheme(theme) {
+	localStorage.setItem("theme", theme);
+}
+
+function getTheme() {
+	return localStorage.getItem("theme");
 }
 
 function initTheme() {
-	if (window.matchMedia("(prefers-color-scheme: dark)").matches)
-		toggleTheme();
+	var theme;
+	if (theme = getTheme()) {
+		setTheme(theme);
+	} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		setTheme('dark');
+	}
+}
+
+function saveTheme(theme) {
+	localStorage.setItem("theme", theme);
+}
+
+function getTheme() {
+	return localStorage.getItem("theme");
+}
+
+function initTheme() {
+	var theme = getTheme();
+	if (!theme) {
+		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			setTheme('dark');
+		}
+	}
+	setTheme(theme);
+}
+
+function setTheme(theme) {
+	var icon = (theme === 'light') ? 'bi-toggle-off' : 'bi-toggle-on';
+	document.body.className = theme;
+	document.getElementById("toggle-icon").className = icon;
+	saveTheme(theme);
 }
 
 function toggleTheme() {
-	document.body.classList.toggle("dark");
-	document.getElementById("toggle-icon").classList.toggle('bi-toggle-on');
+	console.log("toggleTheme before", document.body.classList, getTheme());
+}
+
+function setTheme(theme) {
+	var icon = (theme === 'light') ? 'bi-toggle-off' : 'bi-toggle-on';
+	document.body.className = theme;
+	document.getElementById("toggle-icon").className = icon;
+	saveTheme(theme);
+}
+
+function toggleTheme() {
+	console.log("toggleTheme before", document.body.classList, getTheme());
+
+	if (document.body.classList.contains('dark')) {
+		setTheme('light');
+	} else {
+		setTheme('dark');
+	}
+
+	console.log("toggleTheme after", document.body.classList, getTheme());
+
+	// document.body.classList.toggle("dark");
+	// document.getElementById("toggle-icon").classList.toggle('bi-toggle-on');
 }
